e94b3de231b50170b5ed7cc05952d84fe00d6bf3
0529th <101308419+0529th@users.noreply.github.com>
2022-06-22 09:00:34 +0900

    Update connect.php

--- connect/connect.php
+++ connect/connect.php
@@ -1,14 +1,27 @@
-<?php
-    $host = "localhost";
-    $user = "grhimpan2";
-    $pass = "dz!en940318";
-    $db = "grhimpan2";
-    $connect = new mysqli($host, $user, $pass, $db);
-    $connect -> set_charset("utf8");
-    
-    if(mysqli_connect_errno()) {
-        echo "DATABASE connect false";
-    } else {
-        // echo "DATABASE connect true";
-    }
+<?php
+
+    $host = "localhost";
+
+    $user = "user";
+
+    $pass = "pass";
+
+    $db = "db";
+
+    $connect = new mysqli($host, $user, $pass, $db);
+
+    $connect -> set_charset("utf8");
+
+    
+
+    if(mysqli_connect_errno()) {
+
+        echo "DATABASE connect false";
+
+    } else {
+
+        // echo "DATABASE connect true";
+
+    }
+
 ?>
